c8ce2c266be7e31d00e709d2ae8e327696fd0e22
Sally <sallybao29@gmail.com>
2015-11-19 22:34:53 -0500

    started who query

--- conchshell.py
+++ conchshell.py
@@ -1,4 +1,32 @@
 import google, urllib2, bs4, re
 
 
-"yeeeah"
+def who(query):
+    pages = api_stuff(query)
+    pattern = "[A-Z]{1}[a-z]{2,} [A-Z]{1}[a-z]{1,}"
+    d = {}
+    for page in pages:
+        result = re.findall(pattern, page)
+        for name in result:
+            if name not in d:
+                d["name"] = 1
+            else:
+                d["name"] += 1
+
+
+def get_max(d):
+    max_val = max(d.values())
+    keys = []
+    for x,y in d.items():
+        if y == max_val:
+            keys.append(x)
+    return keys
+
+
+if __name__ == "__main__":
+
+    d = { "hell": 2, "potato": 15,
+          "cow": 15, "chain saw": 2 }
+    
+    print get_max(d)
+            
