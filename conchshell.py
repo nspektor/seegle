8003fb816543220526772af32fde006b5e2d6a98
nspektor <nellie.spektor@gmail.com>
2015-11-20 10:27:41 -0500

    api stuff

--- conchshell.py
+++ conchshell.py
@@ -1,10 +1,15 @@
 import google, urllib2, bs4, re
-def api_stuff():
-    pages = google.search(q,num=10,start=0,stop=10)
-    
+def api_stuff(query):
+    pages = google.search(query,num=10,start=0,stop=10)
+    texts = []
     results = []
     for r in pages:
         results.append(r)
         url = urllib2.urlopen(results[r])
         result_page = url.read().decode('ascii')
-        soup
+        soup = bs4.BeautifulSoup(result_page)
+        raw = soup.get_text(result_page)
+        text = re.sub("[\t\n ]+",' ',raw)
+        texts.append(text)
+    return texts
+
