6f2a478239d0c9485309be9a1cafc78826356e56
Masha <maryged@gmail.com>
2015-11-21 10:41:41 -0500

    less stupid version

--- conchshell.py
+++ conchshell.py
@@ -6,16 +6,11 @@ def when(query):
     regexp_std='(?<month>[A-Z][a-z]{2,8}) (?<day>\d{1,2}),? (?<year>\d{1,4})'
     regexp_era='(\d{1,10}) (BC|AD)'
     d={}
+    result=[]
     for page in pages:
-        result_std=findall(regexp_std,page)
-        result_era=findall(regexp_era,page)
-        for name in result_std:
-            if name in d:
-                d[name]+=1
-            else:
-                d[name]=1
-        for name in result_era:
-            if name in d:
-                d[name]+=1
-            else:
-                d[name]=1
+        result=result+findall(regexp_std,page)+findall(regexp_era,page)
+    for name in result:
+        if name in d:
+            d[name]+=1
+        else:
+            d[name]=1
