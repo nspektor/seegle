c40c80ab4d18506246ce04512bd2e43fda3542b8
Masha <maryged@gmail.com>
2015-11-21 10:37:04 -0500

    when

--- conchshell.py
+++ conchshell.py
@@ -1,4 +1,21 @@
 import google, urllib2, bs4, re
 
 
-"yeeeah"
+def when(query):
+    pages=api_stuff(query)
+    regexp_std='(?<month>[A-Z][a-z]{2,8}) (?<day>\d{1,2}),? (?<year>\d{1,4})'
+    regexp_era='(\d{1,10}) (BC|AD)'
+    d={}
+    for page in pages:
+        result_std=findall(regexp_std,page)
+        result_era=findall(regexp_era,page)
+        for name in result_std:
+            if name in d:
+                d[name]+=1
+            else:
+                d[name]=1
+        for name in result_era:
+            if name in d:
+                d[name]+=1
+            else:
+                d[name]=1
