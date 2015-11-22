eb4587afd62c3a7eee0124e5133bd135ee37d53c
Nellie <nellie.spektor@gmail.com>
2015-11-22 16:19:39 -0500

    made results page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
 if __name__=="__main__":
     app.debug = True
     app.run(host='0.0.0.0',port=8000)
