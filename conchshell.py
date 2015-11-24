28eb3e7a7ccb14b753be8fab60a1cd102b0accdf
Nellie <nellie.spektor@gmail.com>
2015-11-24 12:05:30 -0500

    fixing api stuff

--- conchshell.py
+++ conchshell.py
@@ -3,14 +3,17 @@ import google, urllib2, bs4, re
 def api_stuff(query):
     pages = google.search(query,num=10,start=0,stop=10)
     texts = []
-    results = []
+
     for r in pages:
-        results.append(r)
-        url = urllib2.urlopen(results[r])
-        result_page = url.read().decode('ascii')
-        soup = bs4.BeautifulSoup(result_page)
-        raw = soup.get_text(result_page)
-        text = re.sub("[\t\n ]+",' ',raw)
-        texts.append(text)
+        try:
+           
+            url = urllib2.urlopen(r)
+            result_page = url.read().decode('ascii', 'ignore')
+            soup = bs4.BeautifulSoup(result_page, "html_parser")
+            raw = soup.get_text()
+            text = re.sub("[\t\n ]+",' ',raw)
+            texts.append(text)
+        except:
+            pass
     return texts
 
