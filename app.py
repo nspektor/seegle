68b5c50b848fcda2d1ff884f11692679fff3f884
Sally <sallybao29@gmail.com>
2015-11-26 21:02:37 -0500

    getting slower

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 import urllib2
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from conchshell import find_results
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 @app.route("/home", methods=["GET","POST"])
 def home():
+    """ runs home page and from for query input
+    
+    Returns:
+     Home page or result page if query is inputed
+    
+    """
     if request.method == "GET":
         return render_template("home.html")
     else:
         q = request.form.get('query')
+        if(q==""):
+            q="no question entered, try again"
         return redirect(url_for("results",query=q))
-
 @app.route("/results",  methods=["GET","POST"])
 @app.route("/results/<query>",  methods=["GET","POST"])
 def results(query):
+    """Gets query that the user entered in the home page 
+    and displays the answer
+    
+    Arguments:
+     query: a string that is the user's query input
+    Return:
+     html page with answer to query
     """
-    gets query that the user entered in the home page and displays the answer
-    """
+
     try:
-        result = conchshell.find_results(query)
+        result = find_results(query)
         return render_template("results.html", question=query, answer=result, error=False)
     except:
         return render_template("results.html", question=query, answer="nope", error=["your question is wrong"])
--- conchshell.py
+++ conchshell.py
@@ -1,11 +1,12 @@
 import google, urllib2, bs4, re
+from threading import Thread
+from collections import Counter
 
 
 stop_words = []
 
 def load_stop_words():
-    """
-    reads stop-words into dictionary from static
+    """reads stop-words into dictionary from static
     """
     global stop_words
     file = open("static/stop-word-list.csv", 'r')
@@ -14,37 +15,76 @@ def load_stop_words():
         stop_words += l
     file.close()
 
+
+def process_page(page, l):
+    """Opens url and uses Beautiful Soup to extract text
+    
+    Arguments:
+      page: a url string
+      l: the list to append the result to
+    """
+    text = None
+    try:
+        url = urllib2.urlopen(page)
+        result_page = url.read().encode("ascii", 'ignore')
+        soup = bs4.BeautifulSoup(result_page, "html.parser")
+        raw = soup.get_text()
+        text = re.sub("[\t\n ]+",' ', raw)
+    except:
+        pass
+
+    if text:
+        l.append(text)
     
 def api_stuff(query):
-    pages = google.search(query,num=10,start=0,stop=10)
-    texts = []
-    for r in pages:
-        try:
-            url = urllib2.urlopen(r)
-            result_page = url.read().encode("ascii", 'ignore')
-            soup = bs4.BeautifulSoup(result_page, "html.parser")
-            raw = soup.get_text()
-            text = re.sub("[\t\n ]+",' ', raw)
-            texts.append(text)
-        except:
-            pass
-    return texts
+    """Extract text from several urls using threads
+    
+    Arguments:
+      query: a string containing a question
 
+    Returns:
+      texts: a list of text strings, one for each url page
+    """
+    pages = google.search(query,num=20,start=0,stop=20)
+    threads = []
+    texts = []
+    
+    for page in pages:
+        threads.append(Thread(target = process_page,
+                              args = (page, texts)))
+        
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+ 
+    return texts
 
 
 
 def who(query):
     pages = api_stuff(query)
-    pattern = "[A-Z]{1}[a-z]{2,} [A-Z]{1}[']?[A-Z]?[a-z]{1,}"
-    names = get_max_freq(pages, pattern)
-    #if there is more than one name in names
-    #do some more stuff
-    #if len(names) == 1:
-        #return names[0]
-    l = max_val(names)
-    if not l:
+    pattern = "([A-Z]([a-z]+))?[A-Z]([a-z]+|\.) ([A-Z]')?([A-Z]([a-z]+))?[A-Z]([a-z]+|\.)"
+
+    freq = get_max_freq(pages, pattern)
+    points = get_points(pages, pattern)
+   
+    #print "Pages: %d" % len(pages)
+    l1 = max_val(freq)
+    l2 = max_val(points)
+    """
+    print "By frequency: "
+    print l1
+    print "By points: "
+    print l2
+    """
+    l1 += l2
+
+    if not l1:
         return "I don't know, sorry."
-    return l
+    
+    count = Counter(l1)
+    common = count.most_common(1)
+    
+    return common[0][0]
 
 
 def when(query):
@@ -67,8 +107,8 @@ def when(query):
     return l
 
 
-def contains(string, l):
-    words = string.split(" ")
+def contains(phrase, l):
+    words = phrase.split(" ")
     for word in words:
         #word = word.lower()
         if word.lower() in l:
@@ -89,17 +129,42 @@ def get_max_freq(pages, pattern):
     """
     d = {}
     for page in pages:
-        result = re.findall(pattern, page)
-        for name in result:
-            #print name
-            if name not in d:
-                d[name] = 1
+        #result = re.findall(pattern, page)
+        it = re.finditer(pattern, page)
+        for match in it:
+            first = match.group(0)
+            if first not in d:
+                d[first] = 1
             else:
-                d[name] += 1
+                d[first] += 1
+
+    for key in d:
+        for page in pages:
+            pattern = pattern[pattern.find(" ")+1:]
+            l = re.findall(pattern, page)
+            words = key.split()
+            for word in words:
+                while (word in l):
+                    d[key] += 1
+                    index = l.index(word)
+                    l.pop(index)
+                
     return d
    
-   
 
+def get_points(pages, pattern):
+    d = get_max_freq(pages, pattern)
+    total_pages = len(pages)
+   
+    for key in d:
+        page_count = 0
+        for page in pages:
+            if key in page:
+                page_count += 1
+        d[key] = (float(page_count) / total_pages) * d[key]
+
+    return d
+    
 
 def max_val(d):
     """finds the key with the largest corresponding value in dictionary d
@@ -116,7 +181,10 @@ def max_val(d):
     for key in d.keys():
         if contains(key, stop_words):
             d.pop(key)
-            
+
+    if not d:
+        return []
+    
     max_val = max(d.values())
     keys = []
     for x,y in d.items():
@@ -146,13 +214,14 @@ def find_results(query):
 if __name__ == "__main__":
 
     #print who("Who wrote The Things They Carried?")
+    #print who("Who created Facebook?")
+    print who("Who played the first Spiderman?")
     #print who("Who said \" Let them eat cake\"?")
     #print find_results("Who played Spiderman?")
-    print find_results("Who killed Julius Caesar?")
+    #print find_results("Who killed Julius Caesar?")
     #print when("When did World War II start?")        
 
-       
-           
+   
            
            
 
